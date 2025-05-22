d9240690d7611c9c946d021e64101580bc7d033f
Anildo Carneiro <anildo.10@hotmail.com>
2025-05-21 22:49:35 -0300

    traduz stdbool.h -> convencional_lógico.h

---
+++ win32/include/convencional_lógico.h
@@ -0,0 +1,11 @@
+#se_não_definido __Convencional_Lógico
+#defina __Convencional_Lógico
+
+/* ISOC99 lógico */
+
+#defina lógico	_Lógico
+#defina verdadeiro	1
+#defina falso	0
+#defina __lógico_verdadeiro_e_falso_estão_definidos 1
+
+#fim_se /* __Convencional_Lógico */
